7d2ff4de5178e99fafca9a1812537ed5271b579d
Jasfer <jasfergalang01@gmail.com>
2021-09-21 05:48:38 +0000

    Compute the Average of the Student

---
+++ Act2.py
@@ -0,0 +1,10 @@
+print("PRINTING THE AVERAGE OF THE STUDENT")
+pre=int(input("Enter Prelim: "))
+mid=int(input("Enter Midterm: "))
+sem=int(input("Enter SemiFinal: "))
+fin=int(input("Enter Final: "))
+
+tot=(pre+mid+sem+fin)
+ave=tot/4
+
+print(ave, "The average ")
