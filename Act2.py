7d30e07d6819807a513988f19cfafcf246d7b2df
Jasfer <jasfergalang01@gmail.com>
2021-09-21 06:07:23 +0000

    Determine the equivalents grades in Letters

--- Act2.py
+++ Act2.py
@@ -13,3 +13,25 @@ if ave>=75:
     print("Passed")
 else:
     print("Failed")
+
+ print("PRINTING THE AVERAGE OF THE STUDENT")
+pre=int(input("Enter Prelim: "))
+mid=int(input("Enter Midterm: "))
+sem=int(input("Enter SemiFinal: "))
+fin=int(input("Enter Final: "))
+
+tot=(pre+mid+sem+fin)
+ave=tot/4
+
+if ave>=99 and ave<=100:
+    print("A")
+elif ave>=90 and ave<=98:
+    print("B")
+elif ave>=80 and ave<=89:
+    print("C")
+elif ave>=71 and ave<=79:
+    print("D")
+elif ave>=61 and ave<=70:
+    print("E")
+else:
+    print("F")   
